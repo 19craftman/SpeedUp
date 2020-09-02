4a2323db711f8e53a8b35e9bc94023846c915730
19craftman <47764907+19craftman@users.noreply.github.com>
2020-09-02 01:53:55 -0400

    Initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float speed = 2;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed * rb.gravityScale, rb.velocity.y);
+    }
+ 
+  void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Speed")
+        {
+            StartCoroutine(speeding());
+            Destroy(collision.gameObject);
+        }
+       
+    }
+    IEnumerator speeding()
+    {
+        speed = 5;
+        yield return new WaitForSeconds(5);
+        speed = 2; 
+
+    }
+}
